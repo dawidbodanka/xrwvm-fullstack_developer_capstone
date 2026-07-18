a3ff9bc5e5e26e5dd979fe1ea0102858018eef0d
Dawid Bodanka <dawidbodanka@gmail.com>
2026-07-18 16:00:01 +0200

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,7 +1,7 @@
 """djangoproj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    [https://docs.djangoproject.com/en/3.2/topics/http/urls/](https://docs.djangoproject.com/en/3.2/topics/http/urls/)
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
-from django.conf.urls.static import static
+
 from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,9 +29,12 @@ urlpatterns = [
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
-    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
-    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
-
-
-
+    path(
+        'dealer/<int:dealer_id>',
+        TemplateView.as_view(template_name="index.html")
+    ),
+    path(
+        'postreview/<int:dealer_id>',
+        TemplateView.as_view(template_name="index.html")
+    ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
